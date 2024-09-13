cbdd966f91250693009750728452b864c508682c
Déborah <dsales15@gmail.com>
2024-09-13 10:19:03 -0300

    feat: productions

---
+++ Etapa 2 - Léxico/CajuLex/src/caju/Main2.java
@@ -0,0 +1,29 @@
+package caju;
+import caju.lexer.*;
+import caju.node.*;
+import java.io.*;
+
+public class Main2
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			String arquivo = "teste/teste.cj";
+
+			Lexer lexer =
+					new Lexer(
+							new PushbackReader(  
+									new FileReader(arquivo), 1024)); 
+			Token token;
+			while(!((token = lexer.next()) instanceof EOF)) {
+				System.out.println(token.getClass());
+				System.out.println(" ( "+token.toString()+")");
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
